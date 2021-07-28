64876b7274f729928d817f0d359c89b914289820
Stephanie La Raja <stephanielaraja@gmail.com>
2021-07-28 09:05:03 -0400

    commit tree a3980c67530088f256158b5e4e99abba361f894d

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,43 @@
+# class BirdsController < ApplicationController
+#   def index
+#     @birds = Bird.all
+#     render json: 'Remember that JSON is just object notation converted to string data, so strings also work here'
+#   end
+# end 
+
+# class BirdsController < ApplicationController
+#   def index
+#     @birds = Bird.all
+#     render json: 'Remember that JSON is just object notation converted to string data, so strings also work here'
+#   end
+# end 
+
+# class BirdsController < ApplicationController
+#   def index
+#     @birds = Bird.all
+#     render json: ['As','well','as','arrays']
+#   end
+# end 
+
+# class BirdsController < ApplicationController
+#   def index
+#     @birds = Bird.all
+#     render json: @birds
+#   end
+# end 
+
+# class BirdsController < ApplicationController
+#   def index
+#     @birds = Bird.all
+#     render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }
+#   end
+# end 
+
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render 'birds/index.html.erb'
+    render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }.to_json
+    #this renders the same thing as above
+    #to_json is not necessary because rails does it automatically
   end
-end
+end 
